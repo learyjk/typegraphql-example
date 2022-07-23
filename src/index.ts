297363a1d3d041eb91258c6923a1242e86f77cbf
Keegan Leary <leary.keegan@gmail.com>
2022-07-23 13:20:55 -0700

    typeorm

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Query, Resolver } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String, { nullable: true })
+  async hello() {
+    return "Hello World";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on http://localhost:4000/graphql");
+  });
+};
+main();
